c49ad3b257483f539356a5bec09ec6a47fee5803
james5418 <james900425@gmail.com>
2023-01-19 23:12:18 +0800

    [feat] Jest setup

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,2 +1,9 @@
 import app from "../src/index";
 
+test('adds 1 + 2 to equal 3', () => {
+    let x: number = 1, y: number = 2;
+    let expected: number = 3;
+    let actual: number = x + y;
+  
+    expect(actual).toBe(expected);
+});
