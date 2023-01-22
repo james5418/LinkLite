b550917fee6d7147ae288ef2d0ef2e20070b7100
james5418 <james900425@gmail.com>
2023-01-22 22:51:39 +0800

    [feat] shortenRouter basic functions

--- src/routes/shorten.ts
+++ src/routes/shorten.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
-import UrlSchema from '../models/Url';
+import { nanoid } from 'nanoid';
+import UrlSchema, { IUrl } from '../models/Url';
 import { client as redisClient } from '../databases/redis';
 import isValidUrl from '../utils/validUrl';
 import { isExpired, newExpiredDate} from '../utils/dateHandler';
@@ -9,16 +10,28 @@ export const shortenRouter = Router();
 
 shortenRouter.post('/', async(req: Request, res: Response): Promise<void> => {
 
-    const date = new Date("2023-01-10T12:54:30.781+00:00");
-    const now = new Date();
+    const { url } = req.body;
 
-    const newUrl = new UrlSchema({
-        longUrl: "https://example.com",
-        shortUrl: "xyz",
-        expiredAt: now,
-    });
+    if(!isValidUrl(url)){
+        res.status(400).send("Invalid URL!");
+    }
 
-    const saveUrl = await newUrl.save();
-    res.status(200).json(saveUrl);
+    const urlRecord = await UrlSchema.findOne({ longUrl : url });
+
+    if(urlRecord){
+        urlRecord.expiredAt = newExpiredDate();
+        const saveUrl = await urlRecord.save();
+        res.status(200).json(saveUrl)
+    }
+    else{
+        const newUrl = new UrlSchema({
+            longUrl: url,
+            shortUrl: nanoid(10),
+            expiredAt: newExpiredDate(),
+        });
+        const saveUrl = await newUrl.save();
+
+        res.status(200).json(saveUrl);
+    }
 
 });
