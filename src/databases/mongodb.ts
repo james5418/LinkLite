e99c8065dd03e26a7f198630d680b10efdeab994
james5418 <james900425@gmail.com>
2023-01-25 22:46:04 +0800

    [refactor] mongoose.connect

--- src/databases/mongodb.ts
+++ src/databases/mongodb.ts
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 export default () => {
     mongoose.set('strictQuery', false);
 
-    mongoose.connect(`${process.env.MONGODB_URI}`).then(
-        () => { console.log('Connected to MongoDB'); },
-    ).catch( err => {
+    mongoose.connect(`${process.env.MONGODB_URI}`).then(() => { 
+        console.log('Connected to MongoDB'); 
+    }).catch(err => {
         console.log(`MongoDB connection error. ${err}`);
     });
 }
