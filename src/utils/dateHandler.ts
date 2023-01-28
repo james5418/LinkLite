a9165a69acc92c63b4a0f020ebe19d56963d7928
james5418 <james900425@gmail.com>
2023-01-29 01:35:30 +0800

    [fix] resolve newExpiredDate() 2/28 bug using dayjs

--- src/utils/dateHandler.ts
+++ src/utils/dateHandler.ts
@@ -1,10 +1,12 @@
+import dayjs from "dayjs";
+
 export const isExpired = (date: Date): boolean => {
     const now = new Date();
     return date <= now
 }
 
 export const newExpiredDate = (): Date => {
-    const d = new Date();
-    d.setMonth(d.getMonth() + 1);
-    return d;
+    const now = dayjs();
+    const newDate = now.add(30, 'day');
+    return newDate.toDate();
 }
