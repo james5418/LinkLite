55b178e1242cf3a15fee0a0cce5f23966d132f89
james5418 <james900425@gmail.com>
2023-01-28 19:23:25 +0800

    [test] add unit tests for redirect api and check api

--- tests/check.test.ts
+++ tests/check.test.ts
@@ -7,20 +7,32 @@ import { client as redisClient } from '../src/databases/redis';
 import { newExpiredDate } from '../src/utils/dateHandler';
 
 
-const urlId: string = nanoid(5);
+const validUrlId: string = "isValid";
+const expiredUrlId: string = "isExpired";
+const nonExistentUrlId: string = "nonExistent";
+
 
 beforeAll(async () => {
     const validUrl = new UrlSchema({
         longUrl: "https://www.example.com",
-        shortUrl: urlId,
+        shortUrl: validUrlId,
         expireAt: newExpiredDate(),
     });
     await validUrl.save();
+
+    const expiredUrl = new UrlSchema({
+        longUrl: "https://www.example.com",
+        shortUrl: expiredUrlId,
+        expireAt: new Date("2022-03-30"),
+    });
+    await expiredUrl.save();
 });
 
+
 afterAll(async () => {
-    await UrlSchema.deleteOne({ shortUrl : urlId });
-    await redisClient.sendCommand(["del", urlId]);
+    await UrlSchema.deleteOne({ shortUrl : validUrlId });
+    await redisClient.sendCommand(["del", validUrlId]);
+    await redisClient.sendCommand(["del", nonExistentUrlId]);
 
     await redisClient.quit();
     mongoose.connection.close();
@@ -28,13 +40,52 @@ afterAll(async () => {
 
 
 describe("GET /api/check/:id", () => {
-    test('check a valid short url', async () => {
-        const response = await request(app).get(`/api/check/${urlId}`);
+    test('check a valid short url (using database)', async () => {
+        const response = await request(app).get(`/api/check/${validUrlId}`);
 
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('longUrl');
         expect(response.body).toHaveProperty('shortUrl');
         expect(response.body).toHaveProperty('expireAt');
         expect(Object.keys(response.body).length).toBe(3);
+    });
+
+    test('check a expired valid short url (using database)', async () => {
+        const response = await request(app).get(`/api/check/${expiredUrlId}`);
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('This URL is expired.');
+    });
+
+    test('check a non-existent short url (using database)', async () => {
+        const response = await request(app).get(`/api/check/${nonExistentUrlId}`);
+        expect(response.status).toBe(404);
+        expect(response.text).toBe(`${nonExistentUrlId} not found`);
+    });
+
+    test('check a valid short url (using cache)', async () => {
+        const response = await request(app).get(`/api/check/${validUrlId}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('longUrl');
+        expect(response.body).toHaveProperty('shortUrl');
+        expect(response.body).toHaveProperty('expireAt');
+        expect(Object.keys(response.body).length).toBe(3);
+    });
+
+    test('check a expired valid short url (using cache)', async () => {
+        await redisClient.hSet(expiredUrlId, [
+            'longUrl', "https://www.example.com",
+            'expireAt', new Date("2022-03-30").toISOString(),
+        ]);
+
+        const response = await request(app).get(`/api/check/${expiredUrlId}`);
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('This URL is expired.');
+    });
+
+    test('check a non-existent short url (using cache)', async () => {
+        const response = await request(app).get(`/api/check/${nonExistentUrlId}`);
+        expect(response.status).toBe(404);
+        expect(response.text).toBe(`${nonExistentUrlId} not found`);
     });
 });
--- tests/redirect.test.ts
+++ tests/redirect.test.ts
@@ -7,29 +7,75 @@ import { client as redisClient } from '../src/databases/redis';
 import { newExpiredDate } from '../src/utils/dateHandler';
 
 
-const urlId: string = nanoid(5);
+const validUrlId: string = "isValid";
+const expiredUrlId: string = "isExpired";
+const nonExistentUrlId: string = "nonExistent";
+
 
 beforeAll(async () => {
     const validUrl = new UrlSchema({
         longUrl: "https://www.example.com",
-        shortUrl: urlId,
+        shortUrl: validUrlId,
         expireAt: newExpiredDate(),
     });
     await validUrl.save();
+
+    const expiredUrl = new UrlSchema({
+        longUrl: "https://www.example.com",
+        shortUrl: expiredUrlId,
+        expireAt: new Date("2022-03-30"),
+    });
+    await expiredUrl.save();
 });
 
+
 afterAll(async () => {
-    await UrlSchema.deleteOne({ shortUrl : urlId });
-    await redisClient.sendCommand(["del", urlId]);
+    await UrlSchema.deleteOne({ shortUrl : validUrlId });
+    await redisClient.sendCommand(["del", validUrlId]);
+    await redisClient.sendCommand(["del", nonExistentUrlId]);
 
     await redisClient.quit();
     mongoose.connection.close();
 });
 
-describe("GET /:id", () => {
-    test('redirect to a valid short url', async () => {
-        const response = await request(app).get(`/${urlId}`);
 
+describe("GET /:id", () => {
+    test('redirect to a valid short url (using database)', async () => {
+        const response = await request(app).get(`/${validUrlId}`);
         expect(response.status).toBe(302);
+    });
+
+    test('redirect to a expired valid short url (using database)', async () => {
+        const response = await request(app).get(`/${expiredUrlId}`);
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('This URL is expired.');
+    });
+
+    test('redirect to a non-existent short url (using database)', async () => {
+        const response = await request(app).get(`/${nonExistentUrlId}`);
+        expect(response.status).toBe(404);
+        expect(response.text).toBe(`${nonExistentUrlId} not found`);
+    });
+
+    test('redirect to a valid short url (using cache)', async () => {
+        const response = await request(app).get(`/${validUrlId}`);
+        expect(response.status).toBe(302);
+    });
+
+    test('redirect to a expired valid short url (using cache)', async () => {
+        await redisClient.hSet(expiredUrlId, [
+            'longUrl', "https://www.example.com",
+            'expireAt', new Date("2022-03-30").toISOString(),
+        ]);
+
+        const response = await request(app).get(`/${expiredUrlId}`);
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('This URL is expired.');
+    });
+
+    test('redirect to a non-existent short url (using cache)', async () => {
+        const response = await request(app).get(`/${nonExistentUrlId}`);
+        expect(response.status).toBe(404);
+        expect(response.text).toBe(`${nonExistentUrlId} not found`);
     });
 });
